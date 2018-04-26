4f80f37842a1490eba8a1ac9ab73fb6acb69c871
Kamil <blueflamesxcode@gmail.com>
2018-04-26 13:16:53 +0200

    Add ArrayList facade implementation

---
+++ src/shared/ArrayList.js
@@ -0,0 +1,38 @@
+export class ArrayList {
+    constructor() {
+        this.container = [];
+    }
+
+    add(value) {
+        this.container.push(value);
+    }
+
+    removeAt(index) {
+        if (index < 0 || index >= this.container.length)
+            throw "Index out of container bounds";
+
+        this.container.splice(index, 1);
+    }
+
+    remove(value) {
+        let foundIndex = this.container.indexOf(value);
+
+        if (foundIndex !== -1) {
+            this.removeAt(foundIndex);
+        }
+    }
+
+    iterator() {
+        let index = 0;
+
+        return {
+            next: () => {
+                return this.container[index++];
+            },
+
+            hasNext: () => {
+                return index < this.container.length;
+            }
+        }
+    }
+}
