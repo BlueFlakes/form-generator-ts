518bdab51ccf0b76b7e9ad1d5b1530999bda3667
Kamil <blueflamesxcode@gmail.com>
2018-04-29 14:41:33 +0200

    rename class

--- src/survey/question/Question.js
+++ src/survey/question/Question.js
@@ -1,5 +1,5 @@
 import {idGenerator} from "../../shared/IdGenerator.js";
-import {ShortTextInput} from "../../simpleElements/ShortTextInput.js";
+import {TextInput} from "../../simpleElements/TextInput.js";
 
 export class Question {
     constructor(id, questionView, questionType) {
@@ -23,7 +23,7 @@ export class Question {
 
     addSimpleElement() {
         let uniqueId = idGenerator.nextId();
-        let simpleElement = new ShortTextInput(uniqueId);
+        let simpleElement = new TextInput(uniqueId);
 
         this._simpleElements.set(uniqueId, simpleElement);
         this._questionView.injectSimpleElement(simpleElement);
