a4c8dd98a31bf4daa20de531f8cebda106055ea1
Kamil <blueflamesxcode@gmail.com>
2018-04-30 17:50:43 +0200

    add ListElement

---
+++ src/formGenerator/simpleElements/ListElement.js
@@ -0,0 +1,19 @@
+import {SimpleElement} from "./SimpleElement.js";
+
+export class ListElement extends SimpleElement {
+
+    generateNode(currentIndex) {
+        let currentNode = super.generateNode();
+
+        let label = document.createElement('label');
+        label.for = currentNode.id;
+        label.textContent = currentIndex + 1;
+
+        let simpleContainer = document.createElement('div');
+
+        simpleContainer.appendChild(label);
+        simpleContainer.appendChild(currentNode);
+
+        return simpleContainer;
+    }
+}
