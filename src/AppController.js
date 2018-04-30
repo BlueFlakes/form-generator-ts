5f69324a9f99c02d6747ed253d1c92895e92dac4
Kamil <blueflamesxcode@gmail.com>
2018-04-30 16:21:19 +0200

    separate responsible methods for creating elements from main controller

--- src/AppController.js
+++ src/AppController.js
@@ -1,19 +1,9 @@
-import {Question} from "./formGenerator/survey/question/Question.js";
-import {QuestionView} from "./formGenerator/survey/question/QuestionView.js";
 import {idGenerator} from "./shared/IdGenerator.js";
 import {
-    FieldGeneratorStrategyIdentity,
-    FieldGeneratorStrategies
+    FieldGeneratorStrategyIdentity
 } from "./formGenerator/simpleElements/FieldGeneratorStrategies.js";
 import {Form} from "./formGenerator/survey/Form.js";
-import {SectionView} from "./formGenerator/survey/section/SectionView.js";
-import {Section} from "./formGenerator/survey/section/Section.js";
-import {ContainerView} from "./formGenerator/survey/universalContainer/ContainerView.js";
-import {Container} from "./formGenerator/survey/universalContainer/Container.js";
-import {Button} from "./formGenerator/simpleElements/Button.js";
-import {TitleInput} from "./formGenerator/simpleElements/TitleInput.js";
-import {SectionEnum} from "./formGenerator/survey/section/Section.js";
-
+import * as FormCreator from "./formGenerator/FormCreators.js";
 
 const ApplicationController = (function () {
     const form = new Form();
@@ -25,18 +15,20 @@ const ApplicationController = (function () {
         });
 
         document.getElementById('add__textInput').addEventListener('click', function () {
-            let question = createQuestion(idGenerator.nextId(), FieldGeneratorStrategyIdentity.none);
-            addNewQuestionIntoSurvey(question, []);
+            let question = FormCreator.createQuestion(idGenerator.nextId(), FieldGeneratorStrategyIdentity.none);
+            handle(question, []);
         });
 
         document.getElementById('add__datetime').addEventListener('click', function () {
-            let question = createQuestion(idGenerator.nextId(), FieldGeneratorStrategyIdentity.datetimeBoxStrategy);
-            addNewQuestionIntoSurvey(question, []);
+            let question = FormCreator.createQuestion(idGenerator.nextId(),
+                                                      FieldGeneratorStrategyIdentity.datetimeBoxStrategy);
+            handle(question, []);
         });
 
         document.getElementById('add__textArea').addEventListener('click', function () {
-            let question = createQuestion(idGenerator.nextId(), FieldGeneratorStrategyIdentity.textAreaStrategy);
-            addNewQuestionIntoSurvey(question, []);
+            let question = FormCreator.createQuestion(idGenerator.nextId(),
+                                                      FieldGeneratorStrategyIdentity.textAreaStrategy);
+            handle(question, []);
         });
 
         document.getElementById('delete__form').addEventListener('click', function () {
@@ -45,76 +37,30 @@ const ApplicationController = (function () {
         });
 
         document.getElementById('add__radioQuestion').addEventListener('click', function () {
-            let question = createQuestion(idGenerator.nextId(), FieldGeneratorStrategyIdentity.radioBoxStrategy);
-            let btn = createButton('radio', question);
+            let question = FormCreator.createQuestion(idGenerator.nextId(),
+                                                      FieldGeneratorStrategyIdentity.radioBoxStrategy);
 
-            addNewQuestionIntoSurvey(question, [btn]);
+            let btn = FormCreator.createButton('radio', question);
+
+            handle(question, [btn]);
         });
 
         document.getElementById('add__checkbox').addEventListener('click', function () {
-            let question = createQuestion(idGenerator.nextId(), FieldGeneratorStrategyIdentity.checkBoxStrategy);
-            let btn = createButton('checkbox', question);
+            let question = FormCreator.createQuestion(idGenerator.nextId(),
+                                                      FieldGeneratorStrategyIdentity.checkBoxStrategy);
 
-            addNewQuestionIntoSurvey(question, [btn]);
+            let btn = FormCreator.createButton('checkbox', question);
+
+            handle(question, [btn]);
         })
     }
-
-    function createButton(buttonName, question) {
-        return new Button(idGenerator.nextId(), buttonName, () => question.addSimpleElement());
-    }
     
-    function addNewQuestionIntoSurvey(question, options) {
-
-        let specialOptions = createSimpleContainer(idGenerator.nextId(), 'spec');
-
-        options.forEach((opt) => {
-            specialOptions.putNode(opt.getId(), opt);
-        });
-
-        let titleContainer = createSimpleContainer(idGenerator.nextId(), 'title');
-        let titleInput = new TitleInput(idGenerator.nextId());
-        titleContainer.putNode(titleInput.getId(), titleInput);
-
-        let section = createSection(idGenerator.nextId(), 'survey');
-        section.addToSectionBody(SectionEnum.TitleSection, titleContainer);
-        section.addToSectionBody(SectionEnum.QuestionSection, question);
-        section.addToSectionBody(random(), specialOptions);
+    function handle(question, options) {
+        let section = FormCreator.createFilledSection(question, options);
 
         section.inject();
-
         question.addSimpleElement();
 
         form.put(section.getId(), section);
-    }
-    
-    function random() {
-        // random mean that we are not interested in this ID but still we don't want any collision
-        return idGenerator.nextId();
-    }
-
-    function createQuestion(id, type) {
-        let fieldGenerator = FieldGeneratorStrategies.createFieldGeneratorByIdentity(type);
-
-        let questionViewCreator = function (viewId) {
-            return new QuestionView(viewId);
-        };
-
-        return new Question(id, questionViewCreator, type, fieldGenerator);
-    }
-
-    function createSection(id, mainContainerIdentity) {
-        let sectionViewCreator = (id) => {
-            return new SectionView(id, mainContainerIdentity);
-        };
-
-        return new Section(id, sectionViewCreator);
-    }
-    
-    function createSimpleContainer(id, containerIdentity) {
-        let containerViewCreator = (id) => {
-            return new ContainerView(id);
-        };
-
-        return new Container(id, containerIdentity, containerViewCreator);
     }
 })();
