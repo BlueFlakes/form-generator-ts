cdf14040870746a22b0e1a89c26d555b93be3e06
Kamil <blueflamesxcode@gmail.com>
2018-05-02 20:01:39 +0200

    add params flattening function into contextAdder to fix bug

--- src/shared/Common.js
+++ src/shared/Common.js
@@ -1,5 +1,18 @@
 export const contextAdder = function (fn, ...ctx) {
     return function (...args) {
-        return fn(...ctx, ...args);
+        let flattenParams = flatten([...ctx, ...args]);
+        return fn(flattenParams);
     };
 };
+
+function flatten(arr, result = []) {
+    for (let i = 0, length = arr.length; i < length; i++) {
+        const value = arr[i];
+        if (Array.isArray(value)) {
+            flatten(value, result);
+        } else {
+            result.push(value);
+        }
+    }
+    return result;
+}
