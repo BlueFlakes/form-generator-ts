a9f345b63bc6e272c0c654ba545b166c555db36d
Kamil <blueflamesxcode@gmail.com>
2018-05-02 21:27:49 +0200

    fix bug with too deep nested arrays flattening

--- src/shared/Common.js
+++ src/shared/Common.js
@@ -1,21 +1,8 @@
 export const contextAdder = function (fn, ...ctx) {
     return function (...args) {
-        let flattenParams = flatten([ctx, args]);
-        return fn(flattenParams);
+        return fn([...ctx, ...args]);
     };
 };
-
-function flatten(arr, result = []) {
-    for (let i = 0, length = arr.length; i < length; i++) {
-        const value = arr[i];
-        if (Array.isArray(value)) {
-            flatten(value, result);
-        } else {
-            result.push(value);
-        }
-    }
-    return result;
-}
 
 export function instanceConstructor(Constructor) {
     return function (params) {
