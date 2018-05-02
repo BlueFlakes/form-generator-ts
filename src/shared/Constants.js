b8f8cc8540b75c9a7365b93787abdb343960b129
Kamil <blueflamesxcode@gmail.com>
2018-05-02 12:40:21 +0200

    upgrade DOM userinterface communication :p

--- src/shared/Constants.js
+++ src/shared/Constants.js
@@ -1,3 +1,27 @@
-export const OnEvent = Object.freeze({
-    click: "click"
-});
+export const DOM = (function () {
+
+    return Object.freeze({
+        attach: Object.freeze({
+            event: attachEvent,
+            child: attachChild
+        }),
+
+        OnEvent: Object.freeze({
+            click: "click"
+        })
+    });
+
+    function attachEvent(identity, onEvent, task) {
+        let node = docSelector(identity);
+        node.addEventListener(onEvent, task);
+    }
+    
+    function attachChild(identity, deliveredChild) {
+        let node = docSelector(identity);
+        node.appendChild(deliveredChild);
+    }
+    
+    function docSelector(identity) {
+        return document.querySelector(identity);
+    }
+}());
