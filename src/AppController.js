a66c65295cb0007f4693cd63dc9bb5c53f342453
Kamil <blueflamesxcode@gmail.com>
2018-05-02 12:56:23 +0200

    refactor AppController with JSHINT

--- src/AppController.js
+++ src/AppController.js
@@ -1,84 +1,87 @@
-import {idGenerator} from "./shared/IdGenerator.js";
 import {
     FieldGeneratorStrategyIdentity
 } from "./formGenerator/simpleElements/FieldGeneratorStrategies.js";
 import {Form} from "./formGenerator/survey/Form.js";
 import * as FormCreator from "./formGenerator/FormCreators.js";
+import {DOM} from "./shared/Constants.js";
 
 const ApplicationController = (function () {
+    "use strict";
     const form = new Form();
 
-    {
-        document.getElementById('render__form').addEventListener('click', function () {
+    (function attachEventListeners() {
+
+        DOM.attach.event("#render__form", DOM.OnEvent.click, function () {
             form.render();
         });
 
-        document.getElementById('add__textInput').addEventListener('click', function () {
-            let question = FormCreator.createQuestion(idGenerator.nextId(), FieldGeneratorStrategyIdentity.simpleStrategy);
-            handle(question, []);
-        });
-
-        document.getElementById('add__datetime').addEventListener('click', function () {
-            let question = FormCreator.createQuestion(idGenerator.nextId(),
-                                                      FieldGeneratorStrategyIdentity.datetimeBoxStrategy);
-            handle(question, []);
-        });
-
-        document.getElementById('add__textArea').addEventListener('click', function () {
-            let question = FormCreator.createQuestion(idGenerator.nextId(),
-                                                      FieldGeneratorStrategyIdentity.textAreaStrategy);
-            handle(question, []);
-        });
-
-        document.getElementById('delete__form').addEventListener('click', function () {
+        DOM.attach.event("#delete__form", DOM.OnEvent.click, function () {
             form.clearSections();
         });
 
-        document.getElementById('add__enumeratedList').addEventListener('click', function () {
-            let question = FormCreator.createQuestion(idGenerator.nextId(),
-                                                      FieldGeneratorStrategyIdentity.enumeratedListStrategy);
+        (function attachQuestionCreators() {
 
-            let btn = FormCreator.createButton('option', question, () => {
-                question.addSimpleElement();
+            DOM.attach.event("#add__textInput", DOM.OnEvent.click, function () {
+                let question = FormCreator.createQuestion(FieldGeneratorStrategyIdentity.simpleStrategy);
+                handle(question, []);
+            });
+
+            DOM.attach.event("#add__datetime", DOM.OnEvent.click, function () {
+                let question = FormCreator.createQuestion(FieldGeneratorStrategyIdentity.datetimeBoxStrategy);
+                handle(question, []);
+            });
+
+            DOM.attach.event("#add__textArea", DOM.OnEvent.click, function () {
+                let question = FormCreator.createQuestion(FieldGeneratorStrategyIdentity.textAreaStrategy);
+                handle(question, []);
+            });
+
+            DOM.attach.event("#add__enumeratedList", DOM.OnEvent.click, function () {
+                let question = (function () {
+                    let strategyIdentity = FieldGeneratorStrategyIdentity.enumeratedListStrategy;
+                    return FormCreator.createQuestion(strategyIdentity);
+                }());
+                
+                let btn = (function (question) {
+                    let btnStrategy = () => {
+                        question.addSimpleElement();
+                        question.reRender();
+                    };
+
+                    return FormCreator.createButton("option", question, btnStrategy);
+                }(question));
+
+                handle(question, [btn]);
                 question.reRender();
             });
 
-            handle(question, [btn]);
-            question.reRender();
-        });
+            DOM.attach.event("#add__rangeList", DOM.OnEvent.click, function () {
+                let question = FormCreator.createQuestion(FieldGeneratorStrategyIdentity.rangeListStrategy);
+                handle(question, []);
+            });
 
-        document.getElementById('add__rangeList').addEventListener('click', function () {
-            let question = FormCreator.createQuestion(idGenerator.nextId(),
-                                                      FieldGeneratorStrategyIdentity.rangeListStrategy);
+            DOM.attach.event("#add__radioQuestion", DOM.OnEvent.click, function () {
+                let question = FormCreator.createQuestion(FieldGeneratorStrategyIdentity.radioBoxStrategy);
+                let btn = FormCreator.createButton("radio", question);
+                handle(question, [btn]);
+            });
 
-            handle(question, []);
-        });
+            DOM.attach.event("#add__checkbox", DOM.OnEvent.click, function () {
+                let question = FormCreator.createQuestion(FieldGeneratorStrategyIdentity.checkBoxStrategy);
+                let btn = FormCreator.createButton("checkbox", question);
+                handle(question, [btn]);
+            });
 
-        document.getElementById('add__radioQuestion').addEventListener('click', function () {
-            let question = FormCreator.createQuestion(idGenerator.nextId(),
-                                                      FieldGeneratorStrategyIdentity.radioBoxStrategy);
+        }());
+    }());
 
-            let btn = FormCreator.createButton('radio', question);
-
-            handle(question, [btn]);
-        });
-
-        document.getElementById('add__checkbox').addEventListener('click', function () {
-            let question = FormCreator.createQuestion(idGenerator.nextId(),
-                                                      FieldGeneratorStrategyIdentity.checkBoxStrategy);
-
-            let btn = FormCreator.createButton('checkbox', question);
-
-            handle(question, [btn]);
-        });
-    }
-    
     function handle(question, options) {
         let section = FormCreator.createFilledSection(question, options);
 
         section.inject();
         question.addSimpleElement();
 
-        form.put(section.getId(), section);
+        let sectionId = section.getId();
+        form.put(sectionId, section);
     }
-})();
+}());
